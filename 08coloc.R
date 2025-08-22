74a706f3c7463b23794fe50acce0e0fc0ed09d79
Claudia Krueger <ckrueger2@luc.edu>
2025-08-22 18:13:12 -0500

    Update 08coloc.R

--- 08coloc.R
+++ 08coloc.R
@@ -103,16 +103,16 @@ for (phenotype in unique_phenotypes) {
       #prepare data for locuscomparer
       pqtl_data <- merged_data %>%
         select(rsid = ID, 
-               chr = CHR,
-               pos = POS, 
-               pval = pval_nominal) %>%
+               chr = as.integer(CHR),
+               pos = as.integer(POS), 
+               pval = as.numeric(pval_nominal)) %>%
         write.table("/tmp/pqtl.tsv", sep="\t", row.names=FALSE, quote=FALSE)
       
       gwas_data_formatted <- merged_data %>%
         select(rsid = ID,
-               chr = CHR, 
-               pos = POS,
-               pval = Pvalue) %>%
+               chr = as.integer(CHR), 
+               pos = as.integer(POS),
+               pval = as.numeric(Pvalue)) %>%
         write.table("/tmp/gwas.tsv", sep="\t", row.names=FALSE, quote=FALSE)
       
       #get lead SNP (most probable colocalization SNP)
@@ -142,10 +142,9 @@ for (phenotype in unique_phenotypes) {
       
       #clean up tmp files
       file.remove("/tmp/pqtl.tsv", "/tmp/gwas.tsv")
+      
       cat("Locuscompare plot saved as:", plot_filename, "\n")
     }
-    
-    cat("\n")
   } else {
     cat("No common variants found for", phenotype, "\n\n")
   }
