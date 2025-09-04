8a50c0c4acabe1946d756b5db201debed69ef141
Claudia Krueger <ckrueger2@luc.edu>
2025-09-04 14:21:03 -0500

    Update 01pull_data_plink.py

--- 01pull_data_plink.py
+++ 01pull_data_plink.py
@@ -22,9 +22,7 @@ bucket = os.getenv('WORKSPACE_BUCKET')
 bucket # gs://fc-secure-bb61452f-d5e2-4d26-9227-6a9444241af8/
 out_path = f"{bucket}/data/plink"
 
-mt_wgs_path = os.getenv("WGS_EXOME_SPLIT_HAIL_PATH")
-mt_wgs_path
-mt_wgs_path = "${wgs_path}/v8/wgs/short_read/snpindel/exome/splitMT/hail.mt"
+mt_wgs_path = "gs://fc-aou-datasets-controlled/v8/wgs/short_read/snpindel/exome/splitMT/hail.mt"
 mt = hl.read_matrix_table(mt_wgs_path)
 intervals = ['chr1:108309567-110397919']
 filt_mt = hl.filter_intervals(mt, [hl.parse_locus_interval(x, reference_genome='GRCh38') for x in intervals])
